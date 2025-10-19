6459c55fb0be2c1f903c65d9e85bb34ac515d710
Tyler Imagbudu <tyler.imagbudu@gmail.com>
2025-10-20 00:48:55 +0200

    Creates BlogContext for managing posts
    Initializes a context for managing blog posts, including fetching them from Supabase and providing loading state. This allows components to easily access and display blog content.

---
+++ src/components/BlogContext/BlogContext.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useContext, useEffect } from "react";
+import type { Posts } from "../../../types.ts";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_PUBLIC_SUPABASE_ANON);
+
+export interface BlogContext {
+    posts:Posts[],
+    loading:boolean
+}
+
+export const BlogProvider = ({children}: {children: React.ReactNode}) => {
+  const [posts, setPosts] = useState<Posts[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+      getPosts();
+    }, []);
+  
+    const getPosts = async () => {
+      try {
+        const { data } = await supabase.from("BlogPosts").select();
+        setPosts(data as Posts[]);
+      } catch (error) {
+        console.log(error);
+        setPosts([]);
+      }
+    };
+
+  return (
+      <BlogContext.Provider value={{posts: posts, loading: loading}}>
+          {children}
+      </BlogContext.Provider>
+  )
+}
+
+export const BlogContext = React.createContext<BlogContext>({posts: [], loading: false});
